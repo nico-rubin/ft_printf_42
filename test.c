a7fb4e137a9d6b5ade1f873babac00212233df3b
Nicolas RUBIN <nrubin@e2r2p18.42.fr>
2021-05-27 11:07:18 +0200

    updating test file

--- test.c
+++ test.c
@@ -33,20 +33,22 @@ int main(void)
     printf("%%\n");
 
     // WIDTH and FLAGS
-    printf("%10d\n", 5);
+    printf("%10d\n", 5); // The width is the minimum number of characters to output. 
 
     printf("%*d\n", 10, 5); // A field width may be '*' instead of a digit string in which case an argument supplies the width.
 
     // 0
-    printf("%010d\n", 5);
+    printf("%010d\n", 5); // A leading '0' functions like the width field with the ' 's replaced by '0'.
+
+    printf("%0*d\n", 10, 5); // A field widht may be '*' instead of a digit string in which case an argument supplies the width.
 
     // -
-    printf("%-10d\n", 5);
+    printf("%-10d\n", 5); // The '-' flag functions in conjunction with the width field, left aligning the result.
 
     printf("%-010d\n", 5); // Flag '0' is ignored when flag '-' is present.
     
     // .
-    printf("%.3s\n", "Hello there");
+    printf("%.3s\n", "Hello there"); // The '.' specifies the maximum limit on the output.
 
     printf("%.s\n", "Hello there"); // If the digit string is missing the precision is treated as 0.
 
