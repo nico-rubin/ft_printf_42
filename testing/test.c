b3885b8a7a5e5815b4c626b3f3d4d115322bf093
Nicolas RUBIN <nrubin@e3r4p18.42.fr>
2021-05-27 12:51:06 +0200

    push

--- testing/test.c
+++ testing/test.c
@@ -6,6 +6,7 @@ int main(void)
 
     // c
     printf("%c\n", 'c');
+    
 
     // s
     printf("%s\n", "Hello");
@@ -33,24 +34,36 @@ int main(void)
     printf("%%\n");
 
     // WIDTH and FLAGS
-    printf("%10d\n", 5); // The width is the minimum number of characters to output. 
+    printf("%10d\n", 5); // In no case does a non-existant or small field width cause truncation of a field.
 
     printf("%*d\n", 10, 5); // A field width may be '*' instead of a digit string in which case an argument supplies the width.
 
     // 0
     printf("%010d\n", 5); // A leading '0' functions like the width field with the ' 's replaced by '0'.
 
-    printf("%0*d\n", 10, 5); // A field widht may be '*' instead of a digit string in which case an argument supplies the width.
+    printf("%0*d\n", 10, 5); // A field width may be '*' instead of a digit string in which case an argument supplies the width.
 
     // -
-    printf("%-10d\n", 5); // The '-' flag functions in conjunction with the width field, left aligning the result.
+    printf("%-10d\n", 5); // The '-' flag functions in conjunction with the width field, left aligns the result.
 
     printf("%-010d\n", 5); // Flag '0' is ignored when flag '-' is present.
+
+    printf("%0-10d\n", 5); // Flag '0' is ingored when flag '-' is present.
     
-    // .
+    // . 
     printf("%.3s\n", "Hello there"); // The '.' specifies the maximum limit on the output.
 
     printf("%.s\n", "Hello there"); // If the digit string is missing the precision is treated as 0.
 
     printf ("%.*s\n", 3, "Hello there"); // A precision may be '*' instead of a digit string in which case an argument supplies the width.
+
+    printf("Hello there %.*s Nico\n", "Top Gun"); // If the precision is given by '*' but the argument is not an int then the string is NULL.
+
+    printf("Hell there %.*s Nico\n");
+
+    printf("%010.3s\n", "Hello"); // The '0' flag results in undefined behaviour with 's' conversion specifiers.
+
+    printf("%.100s\n", "Hello"); // If the padding 
+
+    printf("%.0d", 0); // ????????????
 }
