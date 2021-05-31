4af3c9da4a0348ff42fb3e5835174c8abbc6e87e
Nicolas RUBIN <nrubin@e3r1p6.42.fr>
2021-05-31 14:00:37 +0200

    dot and width work toget

--- ft_printf.c
+++ ft_printf.c
@@ -95,6 +95,41 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	ft_printf("%10i\n", 0);
-	printf("%10i\n", 0);
-}
+	// str < width && width > dot && dot > str
+	ft_printf("%5.4i", 12);
+	printf("\n%5.4i\n", 12);
+	
+	// str < width && width < dot && dot > str
+	ft_printf("%4.5i", 120);
+	printf("\n%4.5i\n", 120);
+
+	// str > width && width > dot && dot > str
+	ft_printf("%1.4i", 120);
+	printf("\n%1.4i\n", 120);
+
+	// str > width && width < dot && dot > str
+	ft_printf("%3.5i", 1200);
+	printf("\n%3.5i\n", 1200);
+
+	// str < width && width < dot && dot > str
+	ft_printf("%5.3i", 12000);
+	printf("\n%5.3i\n", 12000);
+}	
+// if there is a precision and a width
+	// if width is bigger than precision
+		// if width > ft_strlen
+			// if right align
+				// print the spaces
+				// print the precision
+				// print the number
+			// if left aling
+				// print the precision
+				// print the number
+				// print the spaces
+		// i width < ft_strlen
+			// print 
+	// if width is smaller than precision
+		// print the precision
+		// print the number
+
+// IF STRLEN IS BIGGER THAN PRECISION USE STRLEN OTHERWISE OPPOSSITE
--- int_helper.c
+++ int_helper.c
@@ -9,24 +9,131 @@ void	ft_print_int(t_list flags, va_list args)
 	n = va_arg(args, int);
 	str = ft_itoa(n);
 
+/*
 	// width
-	if (flags.width > 0)
+	// The width argument is a non-negative decimal integer that controls the minimum number of 
+	// characters that are output. If the number of characters in the output value is less than
+	// the specified width, blanks are added to the left or the right of the values—depending
+	// on whether the left-alignment flag (-) is specified—until the minimum width is reached.
+	// The width specification never causes a value to be truncated. 
+	// If the number of characters in the output value is greater than the specified width, 
+	// or if width isn't provided, all characters of the value are output, 
+	// subject to the precision specification.
+
+	if (flags.width > 0 && flags.dot == -1)
 	{
-		i = flags.width - ft_strlen(str);
-		if (flags.minus == 0)
+		if (flags.width > ft_strlen(str))
 		{
-			while (i--)
+			i = flags.width - ft_strlen(str);
+			if (flags.minus == 0)
+			{
+				while (i--)
+						printf(" ");
+				printf("%i\n", n);
+			}
+			else
+			{
+				printf("%i", n);
+				while (i--)
+				{
 					printf(" ");
+				}
+				printf("\n");
+			}
+		}
+		else
+			printf("%i\n", n);
+	}
+
+	// precision
+	// The precision specifies the minimum number of digits to be printed. If the number
+	// of digits in the argument is less than precision, the output value is padded on the
+	// left with zeros. The value isn't truncated when the number of digits exceeds precision.
+	int j;
+
+	if (flags.width == 0 && flags.dot > -1)
+	{
+		j = flags.dot - ft_strlen(str);
+		if (ft_strlen(str) < flags.dot)
+		{
+			while (j--)
+				printf("0");
 			printf("%i\n", n);
 		}
 		else
-		{
-			printf("%i", n);
-			while (i--)
-			{
-				printf(" ");
-			}
-			printf("\n");
-		}
+			printf("%i\n", n);
 	}
 }
+*/ 
+int k;
+int y; 
+int z;
+
+if (flags.width > 0 && flags.dot > -1)
+{
+	if (flags.width > flags.dot)
+	{
+		if (flags.dot > ft_strlen(str))
+		{
+			k = flags.width - flags.dot;
+			while (k--)
+				printf(" ");
+			y = flags.dot - ft_strlen(str);
+			while (y--)
+				printf("0");
+			printf("%i", n);
+		}
+		if (flags.dot <= ft_strlen(str))
+		{
+			k = flags.width - ft_strlen(str);
+			while (k--)
+				printf(" ");
+			printf("%i", n);
+		}
+	}
+
+	if (flags.width <= flags.dot)
+	{
+		if (flags.dot > ft_strlen(str))
+		{
+			k = flags.dot - ft_strlen(str);
+			while (k--)
+				printf("0");
+			printf("%i", n);
+		}
+		if (flags.dot <= ft_strlen(str))
+			printf("%i", n);
+	}
+}
+}
+
+// IF flags.width AND flags.precison EXIST
+
+	// IF flags.width > flags.precison
+
+		// IF flags.precision > ft_strlen(str)
+
+			// PRINT flags.width
+
+			// PRINT flags.precison
+
+			// PRINT str
+
+		// IF flags.precison <= ft_strlen(str)
+
+			// PRINT flags.width
+
+			// PRINT str
+	
+	// IF flags.width <= flags.precison
+
+		// IF flags.precison > ft_strlen(str)
+
+			// PRINT flags.precison
+
+			// PRINT str
+
+		// IF flags.precison <= ft_stlren(str)
+
+			// PRINT str
+
